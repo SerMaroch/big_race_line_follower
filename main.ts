5da1735294afdc0df16e6ac24a47e563e559370c
SerMaroch <117520156+SerMaroch@users.noreply.github.com>
2025-01-09 17:30:44 +0500

    Обновить pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,28 @@
 function по_линии () {
     if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
-        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.Black)) {
             StartbitV2.startbit_setMotorSpeed(70, 70)
-        } else if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
-            StartbitV2.startbit_setMotorSpeed(70, 30)
         } else if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.Black)) {
             StartbitV2.startbit_setMotorSpeed(30, 70)
+        } else if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
+            StartbitV2.startbit_setMotorSpeed(70, 30)
         } else {
         	
         }
     } else {
-        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.Black)) {
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
+            StartbitV2.startbit_setMotorSpeed(60, -60)
+            basic.pause(50)
+        } else if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.Black)) {
             StartbitV2.startbit_setMotorSpeed(-60, 60)
-            basic.pause(200)
-        } else if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
-            StartbitV2.startbit_setMotorSpeed(-60, 60)
-            basic.pause(200)
+            basic.pause(50)
         } else {
             инверсия = 1
         }
     }
 }
 function линия_с_инверсией () {
-    for (let index = 0; index < 8; index++) {
+    for (let index = 0; index < 3; index++) {
         while (инверсия == 0) {
             по_линии()
         }
@@ -33,7 +33,7 @@ function линия_с_инверсией () {
 }
 function по_белой_линии () {
     if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.Black)) {
-        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.Black)) {
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
             StartbitV2.startbit_setMotorSpeed(70, 70)
         } else if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.Black)) {
             StartbitV2.startbit_setMotorSpeed(70, 30)
@@ -44,11 +44,11 @@ function по_белой_линии () {
         }
     } else {
         if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.Black) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
-            StartbitV2.startbit_setMotorSpeed(-60, 60)
-            basic.pause(200)
+            StartbitV2.startbit_setMotorSpeed(0, 60)
+            basic.pause(50)
         } else if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White) && StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.Black)) {
-            StartbitV2.startbit_setMotorSpeed(-60, 60)
-            basic.pause(200)
+            StartbitV2.startbit_setMotorSpeed(60, 0)
+            basic.pause(50)
         } else {
             инверсия = 0
         }
@@ -68,23 +68,12 @@ function объезд () {
     StartbitV2.startbit_setMotorSpeed(40, 60)
     basic.pause(500)
 }
-let расстояние2 = 0
-let расстояние1 = 0
 let инверсия = 0
 StartbitV2.startbit_Init()
 StartbitV2.lineFollowSensor_init(StartbitV2.startbit_lineFollowPort.port1)
 StartbitV2.ultrasonic_init(StartbitV2.startbit_ultrasonicPort.port2)
 инверсия = 0
 basic.forever(function () {
-    расстояние1 = StartbitV2.startbit_ultrasonic()
-    if (расстояние1 < 15) {
-        StartbitV2.startbit_setMotorSpeed(0, 0)
-        basic.pause(200)
-        расстояние2 = StartbitV2.startbit_ultrasonic()
-        if (расстояние1 - расстояние2 > 0) {
-            объезд()
-        }
-    } else {
-        по_линии()
-    }
+    линия_с_инверсией()
+    StartbitV2.startbit_setMotorSpeed(0, 0)
 })
